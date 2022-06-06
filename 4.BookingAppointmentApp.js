91f7578c2eec2f429d6521ca10abaff548fa9a8b
RahulKurudigi <105605410+RahulKurudigi@users.noreply.github.com>
2022-06-06 15:33:34 +0530

    Update 4.BookingAppointmentApp.js

--- 4.BookingAppointmentApp.js
+++ 4.BookingAppointmentApp.js
@@ -9,51 +9,58 @@ function addUser(event) {
   event.preventDefault();
   if (Name.value === "" || Password.value === "") {
     alert("Please enter name and password");
+  } else {
+    axios({
+      method: "post",
+      url: "https://crudcrud.com/api/b8a9a0b0179c47b9b4c1954735821d5b/appointmentApp",
+      data: {
+        Name: Name.value,
+        Password: Password.value,
+      },
+    })
+      .then(() => displayUsers)
+      .catch((err) => console.log(err));
+    Name.value = "";
+    Password.value = "";
   }
-  axios({
-    method: "post",
-    url: "https://crudcrud.com/api/e7bd2dc56e0b4f1f83eade001ff159c7/appointmentApp",
-    data: {
-      Name: Name.value,
-      Password: Password.value,
-    },
-  })
-    .then((res) => displayUsers)
-    .catch((err) => console.log(err));
-  Name.value = "";
-  Password.value = "";
-  //   displayUsers();
 }
 
 const displayUsers = async () => {
   usersList.innerHTML = "";
   const data = axios.get(
-    "https://crudcrud.com/api/e7bd2dc56e0b4f1f83eade001ff159c7/appointmentApp"
+    "https://crudcrud.com/api/b8a9a0b0179c47b9b4c1954735821d5b/appointmentApp"
   );
   let data1 = await data;
   data1 = data1.data;
+  //   console.log(data1.data);
   console.log(data1);
   data1.forEach((r) => {
+    // console.log(r._id);
     let list = document.createElement("li");
-    list.innerHTML = `${r.Name} , ${r.Password} <button class='delete-btn' onClick='deleteUser(${r._id})' >Delete</button> <button class='edit-btn' onClick='editUser(${r._id},${r.Name},${r.Password}')>Edit</button>`;
+    console.log(r._id);
+    list.innerHTML = `${r.Name} , ${r.Password} <button class="delete-btn" onClick="deleteUser(${r._id})">Delete</button> <button class="edit-btn" onClick="editUser(${r._id},${r.Name},${r.Password})">Edit</button>`;
     usersList.appendChild(list);
+    console.log(list);
   });
 };
-
 displayUsers();
 
-function deleteUser(id) {
+const deleteUser = (id) => {
   console.log("delete");
   axios({
     method: "delete",
-    url: `https://crudcrud.com/api/e7bd2dc56e0b4f1f83eade001ff159c7/appointmentApp/${id}`,
+    url: `https://crudcrud.com/api/b8a9a0b0179c47b9b4c1954735821d5b/appointmentApp/${id}`,
   })
     .then(displayUsers)
     .catch((err) => console.log(err));
-}
+  displayUsers();
+};
 
 function editUser(id, name, password) {
+  console.log("edit");
   Name.value = name;
   Password.value = password;
+  addUser(id);
+
   deleteUser(id);
 }
